1d734556d053b50d7d1b1cd82e1404ba6731a703
shilov.ilya.2001 <shilov.ilya.2001@DESKTOP-OCU8M48>
2022-12-03 18:44:53 +0300

    Добавьте файлы проекта.

---
+++ ExchangeRateClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateClient.ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.Model = new PlotModel { Title = "Example Plot" };
+            this.Model.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.01, "sin(x)"));
+        }
+
+        public PlotModel Model { get; private set; }
+    }
+}
